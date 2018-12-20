4be24421400d2bcae5c5c714bea4ac791229bdb3
Konstantinos Kontos <kkon@handmadeapps.tech>
2018-12-20 13:51:59 +0200

    milestone commit

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,10 @@ FROM parseplatform/parse-server
 
 LABEL maintainer="kkon@handmadeapps.tech"
 
+COPY parseconfig.json /parse-server/config
+
 # Install SMTP and Filesystem adapters
 RUN npm install --save simple-parse-smtp-adapter
 RUN npm install --save @parse/fs-files-adapter
-
+RUN npm install --save parse-server-mailgun
+RUN npm install --save parse-server-sendgrid-adapter
--- sample_create_app_db_user.js
+++ sample_create_app_db_user.js
@@ -4,3 +4,33 @@ db.createUser({
               "roles": [ { role: "dbOwner", db: "parsedb" } ]
               });
 
+
+emailAdapter: {
+module: "simple-parse-smtp-adapter",
+options: {
+fromAddress: 'your@sender.address',
+user: 'email@email.com',
+password: 'AwesomePassword',
+host: 'your.smtp.host',
+isSSL: true, //True or false if you are using ssl
+port: 465, //SSL port or another port
+name: 'your domain name', //  optional, used for identifying to the server
+    //Somtimes the user email is not in the 'email' field, the email is search first in
+    //email field, then in username field, if you have the user email in another field
+    //You can specify here
+emailField: 'username',
+templates: {
+    //This template is used only for reset password email
+resetPassword: {
+    //Path to your template
+template: __dirname + '/views/email/reset-password',
+    //Subject for this email
+subject: 'Reset your password'
+},
+verifyEmail: {
+template: __dirname + '/views/email/verify-email',
+subject: 'Verify Email'
+}
+}
+}
+}
